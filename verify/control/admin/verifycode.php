f857a8f50b093d4d26c6e4a0737bf73789d1a73b
kom551 <kom551@163.com>
2013-12-03 00:33:19 +0800

    1

--- verify/control/admin/verifycode.php
+++ verify/control/admin/verifycode.php
@@ -12,14 +12,16 @@ class verifycode extends Control
     {
         parent::__construct();
         $this->temp = DEDEAPPTPL.'/admin';
+        $this->arclurd = new Lurd('#@__archives', $this->temp, $this->temp.'/lurd');
         $this->lurd = new Lurd('#@__verifycode', $this->temp, $this->temp.'/lurd');
         $this->lurd->appName = "验证码管理";
         $this->lurd->isDebug = FALSE;  //开启调试模式后每次都会生成模板
         $this->lurd->stringSafe = 2;  //默认1(只限制不安全的HTML[script、frame等]，0--为不限，2--为不支持HTML
+        $this->lurd->AddLinkTable($this->arclurd,'arcid','id','title');
         //获取url
         $this->currurl = GetCurUrl();
         //载入模型
-        $this->answer = $this->Model('verifycode');
+        //$this->answer = $this->Model('verifycode');
     }
 
     function ac_index()
@@ -31,30 +33,30 @@ class verifycode extends Control
     //列出验证码
     function ac_list()
     {
-        $ifcheck = request('ifcheck', '2');
-        $askid = request('askid', '');
-        if($ifcheck == 0)
+        $state = request('state', '2');
+        $arcid = request('arcid', '');
+        if($state == 0)
         {
-            $wherequery = "WHERE ifcheck = 0";
-            $this->lurd->SetParameter('ifcheck',0);
-        }else if($ifcheck == 1){
-            $wherequery = "WHERE ifcheck = 1";
-            $this->lurd->SetParameter('ifcheck',1);
+            $wherequery = "WHERE state = 0";
+            $this->lurd->SetParameter('state',0);
+        }else if($state == 1){
+            $wherequery = "WHERE state = 1";
+            $this->lurd->SetParameter('state',1);
         }else{
             $wherequery = "";
         }
-        if($askid)
+        if($arcid)
         {
-            $wherequery .= "WHERE askid =".$askid;
-            $this->lurd->SetParameter('askid',$askid);
+            $wherequery .= "WHERE arcid =".$arcid;
+            $this->lurd->SetParameter('arcid',$arcid);
         }
-        $orderquery = "ORDER BY id DESC ";
+        $orderquery = "ORDER BY dede_verifycode.id DESC ";
         //指定每页显示数
         $this->lurd->pageSize = 20;
         //指定某字段为强制定义的类型
         $this->lurd->BindType('dateline', 'TIMESTAMP', 'Y-m-d H:i');
         //获取数据
-        $this->lurd->ListData('id,askid,uid,username,dateline,content,ifcheck', $wherequery, $orderquery);
+        $this->lurd->ListData('id', $wherequery, $orderquery);
         exit();
     }
 }
